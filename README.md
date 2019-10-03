660eaf38b0f1b85f7fb644fe22ec290e75c47e98
Florin C <florin.cornea@fortech.ro>
2019-10-03 11:32:09 +0300

    example changes

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Strategy design pattern with php example
 
 "In computer programming, the strategy pattern (also known as the policy pattern) is a behavioral software design pattern that enables selecting an algorithm at runtime. Instead of implementing a single algorithm directly, code receives run-time instructions as to which in a family of algorithms to use." - wikipedia
+
+Utilizarea acestui pattern este recomandata in cazul in care dorim sa schimbam diferite functionalitati la runtime;
+
+In exemplul de aici avem 2 tipuri de moduri de a calatori, cu masina sau barca si dorim sa cunoastem costul si consumul pt distanta parcursa.
+Pentru a nu utiliza "if"-uri si a beneficia de avantajele POO, am creat o interfata pentru Strategy din care vom implementa 2 clase concrete in care facem caluculele specifice pt fiecare tip de calatorie.
--- strategy.php
+++ strategy.php
@@ -3,6 +3,7 @@
 interface TravelStrategy
 {
     public function calculateCostbyKm($km);
+    public function calculateGasbyKm($km);
 }
 
 class TravelByCarStrategy implements TravelStrategy
@@ -11,6 +12,14 @@ class TravelByCarStrategy implements TravelStrategy
     {
         $costForOneKm = 2;
         $travelCost = $km * $costForOneKm . " $";
+
+        return $travelCost;
+    }
+
+    public function calculateGasbyKm($km)
+    {
+        $gasForOneKm = 7.2/100;
+        $travelCost = $km * $gasForOneKm . " l of Gas";
 
         return $travelCost;
     }
@@ -25,27 +34,42 @@ class TravelByBoatStrategy implements TravelStrategy
 
         return $travelCost;
     }
+
+    public function calculateGasbyKm($km)
+    {
+        $gasForOneKm = 127/120;
+        $travelCost = $km * $gasForOneKm . " l of Disel";
+
+        return $travelCost;
+    }
 }
 
 
 interface TravelType
 {
     public function calculateCost($km);
+    public function calculateGas($km);
 }
 
 class CarTravel implements TravelType
 {
     private $strategy;
-    
+
     public function __construct()
     {
         $this->strategy = new TravelByCarStrategy();
     }
-    
+
     public function calculateCost($km)
     {
         $travelCost = $this->strategy->calculateCostbyKm($km);
         echo "Travelling " . $km . " km by car will cost " . $travelCost;
+    }
+
+    public function calculateGas($km)
+    {
+        $gas = $this->strategy->calculateGasbyKm($km);
+        echo "Travelling " . $km . " km, the car will consume " . $gas;
     }
 }
 
@@ -63,6 +87,12 @@ class BoatTravel implements TravelType
         $travelCost = $this->strategy->calculateCostbyKm($km);
         echo "Travelling " . $km . " km by boat will cost " . $travelCost;
     }
+
+    public function calculateGas($km)
+    {
+        $gas = $this->strategy->calculateGasbyKm($km);
+        echo "Travelling " . $km . " km, the boat will consume " . $gas;
+    }
 }
 
 function calculateCost($travelType, $distance) {
@@ -71,5 +101,13 @@ function calculateCost($travelType, $distance) {
     return $travelType->calculateCost($distance);;
 }
 
+function estimateGasNeeds($travelType, $distance) {
+    $travelType = new $travelType();
+
+    return $travelType->calculateGas($distance);
+}
+
 $distance = $_GET["distance"];
 calculateCost('CarTravel', $distance);
+echo '<br>';
+estimateGasNeeds('CarTravel', $distance);
